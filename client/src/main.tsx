d7d47510bf55b075e26468a5e1a63b9b78ea7184
Sorin Chirtoaca <129075840+fikesoft@users.noreply.github.com>
2025-04-16 22:45:02 +0200

    Setup app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { TrpcProvider } from './lib/trpc.tsx';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TrpcProvider>
+      <App />
+    </TrpcProvider>
   </StrictMode>
 );
